3de858673d03eca097bad741662597a4c0f95bb4
Yeahboi12356 <89899637+Yeahboi12356@users.noreply.github.com>
2021-11-02 22:23:47 +0800

    Update nisupgrade.py

--- nisupgrade.py
+++ nisupgrade.py
@@ -1,18 +1,27 @@
 import os
 from time import sleep
-print("\nnew pankages: camera-security,version,default")
-n = input("are you sure to upgrade pankages? y/n: ")
-if n == "y":
-  files = input("your username: ")
-  os.system("git clone https://github.com/Yeahboi12356/update && cp update/terminal.py home/" + files)
-  print("clik y to continue")
-  os.system("rd /s update")
-  print("restarting terminal..")
-  sleep(5)
-  os.system("py start-terminal.py")
-elif n == "n":
-  print("restarting terminal")
-  os.system("py start-terminal.py")
-else:
-    print("number not valid")
-    os.system("py nisupgrade.py")
+import urllib.request
+
+import urllib.request
+
+url = "https://whoamilinix.000webhostapp.com/update.txt"
+file = urllib.request.urlopen(url)
+
+for line in file:
+    decoded_line= line.decode("utf-8").strip()
+    print(decoded_line)
+    n = input("are you sure to upgrade pankages? y/n: ")
+    if n == "y":
+      files = input("your username: ")
+      os.system("git clone https://github.com/Yeahboi12356/update && cp update/terminal.py home/" + files)
+      print("clik y to continue")
+      os.system("rd /s update")
+      print("restarting terminal..")
+      sleep(5)
+      os.system("py start-terminal.py")
+    elif n == "n":
+      print("restarting terminal")
+      os.system("py start-terminal.py")
+    else:
+        print("number not valid")
+        os.system("py nisupgrade.py")
