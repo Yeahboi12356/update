90dded0f864e5f5cdb440e6dc50632ab9c364bb0
Yeahboi12356 <89899637+Yeahboi12356@users.noreply.github.com>
2021-11-03 16:12:09 +0800

    Update nisupgrade.py

--- nisupgrade.py
+++ nisupgrade.py
@@ -13,7 +13,11 @@ for line in file:
     n = input("are you sure to upgrade pankages? y/n: ")
     if n == "y":
       files = input("your username: ")
-      os.system("git clone https://github.com/Yeahboi12356/update && cp update/terminal.py home/" + files + " && cp update/delnis.py delnis.py && py delnis.py)
+      os.system("git clone https://github.com/Yeahboi12356/update && cp update/terminal.py home/" + files )
+      print("clik y to continue")
+      os.system("rd /s update")
+      print("restarting terminal..")
+      sleep(5)
     elif n == "n":
       print("restarting terminal")
       os.system("py start-terminal.py")
