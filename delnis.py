484bac611a3e648b12c2b89828ac9a07a0ab040c
Yeahboi12356 <89899637+Yeahboi12356@users.noreply.github.com>
2021-11-03 16:25:12 +0800

    Update delnis.py

--- delnis.py
+++ delnis.py
@@ -1,5 +1,7 @@
-      print("clik y to continue")
-      os.system("rd /s update")
-      print("restarting terminal..")
-      sleep(5)
+os.system("cp update/nisupgrade nisupgrade.py")
+print("clik y to continue")
+os.system("rd /s update")
+print("restarting terminal..")
+sleep(5)
+os.system("py start-terminal.py") 
       
