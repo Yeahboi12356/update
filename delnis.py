10db12381e0ff8167ffc455ecb4376ce29237d44
Yeahboi12356 <89899637+Yeahboi12356@users.noreply.github.com>
2022-02-24 21:27:56 +0800

    Delete delnis.py

--- delnis.py
+++
@@ -1,9 +0,0 @@
-import os
-from time import sleep
-os.system("cp update/nisupgrade.py nisupgrade.py")
-print("clik y to continue")
-os.system("rd /s update")
-print("restarting terminal..")
-sleep(5)
-os.system("start-terminal.exe")
-      
