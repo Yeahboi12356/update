114064d6f25180cb6caaca2645af09d8d6f3e626
Yeahboi12356 <89899637+Yeahboi12356@users.noreply.github.com>
2022-02-24 21:55:45 +0800

    Add files via upload

---
+++ delnis.py
@@ -0,0 +1,10 @@
+import os
+from time import sleep
+os.system("del nisugrade.py")
+os.system("""copy "update\gisupgrade.py" gisupgrade.py""")
+print("clik y to continue")
+os.system("rd /s update")
+print("restarting terminal..")
+sleep(5)
+os.system("start-terminal.exe")
+      
